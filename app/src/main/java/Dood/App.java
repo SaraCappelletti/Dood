eaeb9b8eb54c47d5ebc16621eca002bd569574d7
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2024-01-03 16:20:56 +0100

    Added new fields to the http response

--- app/src/main/java/Dood/App.java
+++ app/src/main/java/Dood/App.java
@@ -14,6 +14,9 @@ import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -44,16 +47,21 @@ public class App {
                     // Usa AsyncContext per gestire la lettura asincrona
                     exchange.dispatch(executor, () -> {
                         exchange.getRequestReceiver().receiveFullString((exchange1, message) -> {
+                            String requestState = "";
                             try {
                                 String requestBody = message;
 
                                 // Converte la stringa JSON in un oggetto Java
                                 ObjectMapper objectMapper = new ObjectMapper();
                                 JsonNode jsonNode = objectMapper.readTree(requestBody);
+                                requestState = "ACCEPTED";
 
                                 // Estrai i valori da jsonNode
-                                String dtImage = jsonNode.get("dt-image").asText();
-                                String owner = jsonNode.get("owner").asText();
+                                String dtImage = jsonNode.get("digitalTwinImage").asText();
+                                String dtId = jsonNode.get("digitalTwinId").asText();
+                                String ownerId = jsonNode.get("ownerId").asText();
+                                String tags = jsonNode.get("tags").asText();
+                                String dtPort = jsonNode.get("digitalTwinPort").asText();
                                 System.out.println("Image name: " + dtImage);
 
                                 CreateContainerResponse container = dockerClient.createContainerCmd(dtImage)
@@ -61,11 +69,30 @@ public class App {
 
                                 // Avvio del container
                                 dockerClient.startContainerCmd(container.getId()).exec();
+                                requestState = "FULLFILLED";
 
                                 // Stampa dell'ID del container appena creato
-                                exchange.getResponseSender().send("Container ID: " + container.getId());
+                                /*exchange.getResponseSender().send("Container ID: " + container.getId());*/
                                 System.out.println("Container ID: " + container.getId());
+
+                                Map<String, Object> deploymentDescriptor = new LinkedHashMap<>();
+                                deploymentDescriptor.put("digitalTwinImage", dtImage);
+                                deploymentDescriptor.put("digitalTwinId", dtId);
+                                deploymentDescriptor.put("ownerId", ownerId);
+                                deploymentDescriptor.put("tags", tags);
+                                deploymentDescriptor.put("digitalTwinPort", dtPort);
+
+                                Map<String, Object> jsonResponse = new HashMap<>();
+                                jsonResponse.put("containerId", container.getId());
+                                jsonResponse.put("deploymentDescriptor", deploymentDescriptor);
+                                jsonResponse.put("requestState", requestState);
+                                jsonResponse.put("digitalTwinURI", "http://localhost:" + dtPort + "/" + dtId);
+
+                                String jsonString = objectMapper.writeValueAsString(jsonResponse);
+
+                                exchange.getResponseSender().send(jsonString);
                             } catch (Exception e) {
+                                requestState = "REFUSED";
                                 e.printStackTrace();
                                 exchange.setStatusCode(500); // Internal Server Error
                                 exchange.getResponseSender().send("Errore durante l'elaborazione della richiesta");
@@ -92,3 +119,19 @@ public class App {
         System.out.println("Server Undertow in esecuzione. Endpoint: http://localhost:80881create-container");
     }
 }
+
+/*{
+    "containerId":"f31913d0be44f5d3442c1845ec9327c76740a4f9c2adb812c8485270e5a4a854",
+    "deploymentDescriptor":{
+        "digitalTwinImage":"saracappelletti/digital-twin-with-configuration-files",
+        "digitalTwinId":"termometro",
+        "ownerId":"Sara Cappelletti",
+        "tags":[
+            "device"
+        ],
+        "digitalTwinPort":3000
+    },
+    "requestState":"FULLFILLED",
+    "digitalTwinURI" : "http://localhost:3000/termometro"
+}*/
+
